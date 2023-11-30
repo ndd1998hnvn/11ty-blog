0ef28de3b0cccfc05bbbaeaed670489e69b5edfc
ndd1998hnvn <nguyenduydat.hnvn@gmail.com>
2023-11-30 16:32:42 +0700

    update data

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css/styles.css");
   return {
     passthroughFileCopy: true,
-    // dir: {
-    //   data: "_data",
-    // },
+    dir: {
+      data: "_data",
+    },
   };
 };
 const { DateTime } = require("luxon");
