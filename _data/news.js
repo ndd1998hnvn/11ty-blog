6c451d86c0b230c75405cd9d08c04410b6dc6292
ndd1998hnvn <nguyenduydat.hnvn@gmail.com>
2023-11-30 16:15:32 +0700

    fix env

--- _data/news.js
+++ _data/news.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = async function () {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=apple&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=${process.env.API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/everything?q=apple&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
     return response.data;
   } catch (error) {
