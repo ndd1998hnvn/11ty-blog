00e49765f0017558c6e4b8291f2a04886383b739
ndd1998hnvn <nguyenduydat.hnvn@gmail.com>
2023-12-01 11:06:53 +0700

    link detail with post

--- _data/news.js
+++ _data/news.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = async function () {
   const url = `https://newsapi.org/v2/everything?q=apple&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`;
   return EleventyFetch(url, {
-    duration: "5m", // save for 1 minute
+    duration: "720m", // save for 1 minute
     type: "json", // we’ll parse JSON for you
   });
 };
