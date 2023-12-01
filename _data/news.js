105ae806839888bdaade02df5db76d339a731132
ndd1998hnvn <nguyenduydat.hnvn@gmail.com>
2023-12-01 10:01:12 +0700

    update time call api

--- _data/news.js
+++ _data/news.js
@@ -1,13 +1,20 @@
-const axios = require("axios");
+const EleventyFetch = require("@11ty/eleventy-fetch");
 require("dotenv").config();
-
 module.exports = async function () {
-  try {
-    const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=apple&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+  const url = `https://newsapi.org/v2/everything?q=apple&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`;
+  return EleventyFetch(url, {
+    duration: "5m", // save for 1 minute
+    type: "json", // we’ll parse JSON for you
+  });
 };
+
+// module.exports = async function () {
+//   try {
+//     const response = await axios.get(
+//       `https://newsapi.org/v2/everything?q=apple&from=2023-11-29&to=2023-11-29&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+//     );
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
